eeb01889bff3f916ab05542509e70921f6ac798f
Alex Badjun <alexbadjun@gmail.com>
2017-02-10 15:02:45 -0500

    added first review

--- MVC Review Site Project/Models/Review.cs
+++ MVC Review Site Project/Models/Review.cs
@@ -9,16 +9,16 @@ namespace MVC_Review_Site_Project.Models
 {
     public class Review
     {
-        //Our ID keys track of each instance
+        
         [Key]
-        public int ID { get; set; }
+        public int ID { get; set; } //Our key is set as ID
         public string Name { get; set; }
         public DateTime PublishDate { get; set; }
         public int Rating { get; set; }
         public string Description { get; set; }
 
 
-        [ForeignKey("Category")]
+        [ForeignKey("Category")] 
         public int CategoryID { get; set; }
         public virtual Category Category { get; set; }
     }
